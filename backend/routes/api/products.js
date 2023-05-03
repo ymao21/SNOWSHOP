a77fb5f8d1e0d4ebad21807573b08c8b2978cecf
yining <yiningmaomao@gmail.com>
2023-05-03 14:56:33 -0400

    product routes

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { setTokenCookie, restoreUser, requireAuth} = require('../../utils/auth');
-const { User, Song, Album, Comment} = require('../../db/models');
+const { User, Product, Cart, Review} = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors} = require('../../utils/validation');
 const {newError} = require('../../utils/newError');
@@ -8,23 +8,73 @@ const product = require('../../db/models/product');
 const router = express.Router();
 
 
-//get all products
+//get products
 
 router.get("/home", async (req, res, next) => {
 
+    const userId = req.user.id
 
+    const products = await Product.findAll({
+        where: {
+            userId: userId
+        }
+    })
+    return res.json({products})
+})
 
+//get product by id
+router.get("/home/:productId", async (req, res, next) => {
 
+    const {productId} = req.params;
+    const product = await Product.findByPk(productId,{})
 
-    const pagination = {};
-    if (page >= 1 && size >= 1) {
-        pagination.limit = size;
-        pagination.offset = size * (page - 1);
-
+    if(!product){
+        const err = newError("Product couldn't be found", 404)
+        return next(err)
     }
-    const songs = await Song.findAll({
-        ...pagination,
+    return res.json(product)
+})
+
+//create product
+router.post("/home", requireAuth, async (req, res, next) => {
+    const userId = req.user.id
+    const {name, price, type, color, category,description, previewImageUrl} = req.body
+
+    const newProduct = await Product.create({
+        userId,
+        name,
+        price,
+        type,
+        color,
+        category,
+        description,
+        previewImageUrl
     })
 
-    return res.json({songs, page, size})
+    return res.json(newProduct)
+});
+
+//update product
+router.put("/home/:productId" , requireAuth , async (req, res, next) => {
+    const {productId} = req.params;
+    const userId = req.user.id;
+    const {name, price, type, color, category,description, previewImageUrl} = req.body
+    const product = await Product.findByPk(productId);
+
+    if(!product){
+        const err = newError("Product couldn't be found", 404)
+        return next(err)
+    }
+
+    if(userId) product.userId = userId
+    if(name) product.name = name
+    if(price) product.price = price
+    if(type) product.type = type
+    if(color) product.color = color
+    if(category) product.category = category
+    if(description) product.description = description
+    if(previewImageUrl) product.previewImageUrl = previewImageUrl
+
+    await product.save()
+    return res.json(product)
 })
