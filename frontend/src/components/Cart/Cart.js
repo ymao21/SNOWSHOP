e6cbeada77ae588b29fdd1966036bf452e19803b
yining <yiningmaomao@gmail.com>
2023-06-06 16:37:31 -0400

    worked on cart

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,30 +1,79 @@
 import './Cart.css'
-import {loadAllCartThunk} from '../../store/cart'
-
+import { loadAllCartThunk, editQuantity, clearCart } from '../../store/cart'
+import { getProductsThunk } from '../../store/products';
 import { useSelector, useDispatch } from 'react-redux';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
+const Cart = () => {
+  const dispatch = useDispatch();
+  const productObj = useSelector((state) => state.productState);
+  const productArr = Object.values(productObj);
+  const cartItemsArr = useSelector((state) => state.cartState.cartItems);
+  const history = useHistory()
 
-const Cart = ({product}) => {
+  useEffect(() => {
+    dispatch(loadAllCartThunk());
+    dispatch(getProductsThunk());
+  }, [dispatch]);
 
-    const dispatch = useDispatch()
+  const handleQuantityChange = (cartItemId, newQuantity) => {
+    if (newQuantity <= 0) {
+      dispatch(clearCart());
+    } else {
+      dispatch(editQuantity(cartItemId, newQuantity));
+    }
+  };
 
-    const cartItem = useSelector(state => state.cartState)
+  const handleCheckout = () => {
+  history.push('/products')
+  };
 
-    // console.log("componentResponse", cartItem)
-
-    useEffect(() => {
-        dispatch(loadAllCartThunk())
-      }, [dispatch])
-
-
+  const cartItems = cartItemsArr.map((cartItem) => {
+    const product = productArr.find((product) => product.id === cartItem.productId);
+    const totalPrice = (product?.price * cartItem.quantity).toFixed(2);
 
     return (
+      <div key={cartItem.id} className="CartItem">
+        <div className="CartItem__Image">
+          <img src={product.previewImageUrl} alt={product.name} />
+        </div>
+        <div className="CartItem__Details">
+          <h3 className="CartItem__Name">{product.name}</h3>
+          <div className="CartItem__Price">Price: ${totalPrice}</div>
+          <div className="CartItem__Quantity">
+            Quantity:
+            <input
+              type="number"
+              value={cartItem.quantity}
+              onChange={(e) => handleQuantityChange(cartItem.id, Number(e.target.value))}
+              min="1"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  });
 
-        <>cartstuff</>
-    )
+  // Calculate the total price of all cart items
+  const totalPrice = cartItemsArr.reduce((total, cartItem) => {
+    const product = productArr.find((product) => product.id === cartItem.productId);
+    const itemPrice = (product.price * cartItem.quantity);
+    return total + itemPrice;
+  }, 0);
 
-}
+  return (
+    <div className="Cart">
+      <h2 className="Cart__Total">Total: ${totalPrice.toFixed(2)}</h2>
+      {cartItems}
+      <div className="Cart__Buttons">
+        <button className="ContinueShoppingButton" onClick={handleCheckout}>Continue Shopping</button>
+        <button className="CheckoutButton" onClick={handleCheckout}>
+          Checkout Now
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-export default Cart
+export default Cart;
--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,67 +1,82 @@
 import { csrfFetch } from "./csrf";
 
-const LOAD_CART = './cart/LOAD_CART'
-const ADD_CART  = '/cart/ADD_CART'
-const REMOVE_CART = '/cart/REMOVE_CART'
-const EDIT_QUANTITY = '/cart/EDIT_QUANTITY'
-const CLEAR_CART = '/cart/CLEAR_CART'
+const LOAD_CART = '/cart/LOAD_CART';
+const ADD_CART = '/cart/ADD_CART';
+const REMOVE_CART = '/cart/REMOVE_CART';
+const EDIT_QUANTITY = '/cart/EDIT_QUANTITY';
+const CLEAR_CART = '/cart/CLEAR_CART';
 
-
-//action creators
+// Action creators
 export const loadToCart = (products) => {
-    return {
-        type: LOAD_CART,
-        products
-    }
-}
+  return {
+    type: LOAD_CART,
+    products,
+  };
+};
 
 export const addToCart = (product) => {
-   return {
+  return {
     type: ADD_CART,
-    product
-   }
-}
+    product,
+  };
+};
 
+export const editQuantity = (cartItemId, newQuantity) => {
+  return {
+    type: EDIT_QUANTITY,
+    cartItemId,
+    newQuantity,
+  };
+};
+
+export const clearCart = () => {
+  return {
+    type: CLEAR_CART,
+  };
+};
 
 export const loadAllCartThunk = () => async (dispatch) => {
+  const response = await csrfFetch('/api/cart');
 
-    const response = await csrfFetch('/api/cart')
+  if (response.ok) {
+    const cartItems = await response.json();
+    dispatch(loadToCart(cartItems));
+    return cartItems;
+  }
+  return response;
+};
 
-    if(response.ok){
-        const cartItem = await response.json();
-        dispatch(loadToCart(cartItem))
-        // console.log("thunkResponse", cartItem)
-        return cartItem
-    }
-    return response
-
-}
-
-
-const initialState = {};
+const initialState = {
+  cartItems: [],
+};
 
 const cartReducer = (state = initialState, action) => {
-    let newState;
+  switch (action.type) {
+    case LOAD_CART:
+      return {
+        ...state,
+        cartItems: action.products,
+      };
+    case ADD_CART:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.product],
+      };
+    case EDIT_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map((item) =>
+          item.id === action.cartItemId ? { ...item, quantity: action.newQuantity } : item
+        ),
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartItems: [],
+      };
+    default:
+      return state;
+  }
+};
 
-    switch(action.type) {
-        case LOAD_CART:
-            newState = {...state}
-            // console.log(newState)
-
-            // action.cart.forEach((product)=>{
-            //     newState[product.id] = product
-            // });
-            return newState
-
-
-        default:
-            return state;
-
-
-
-
-    }
-
-}
-
-export default cartReducer
+export default cartReducer;
