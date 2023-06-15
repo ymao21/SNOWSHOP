9ab837f2cb35ac149f59646594cd1900f63ca3c9
yining <yiningmaomao@gmail.com>
2023-06-15 15:56:42 -0400

    edited filter

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -1,67 +1,96 @@
 import './ProductList.css';
-import {  useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {  getProductsThunk } from '../../store/products';
+import { getProductsThunk } from '../../store/products';
 import ProductCard from '../ProductCard/ProductCard';
 
-
 const ProductList = () => {
+  const [activeFilter, setActiveFilter] = useState('all');
+  const [activeWomenCategory, setActiveWomenCategory] = useState('all');
+  const [activeMenCategory, setActiveMenCategory] = useState('all');
+  const dispatch = useDispatch();
 
-   const [activeFilter, setActiveFilter] = useState('all');
-    const dispatch = useDispatch();
+  const productObj = useSelector((state) => state.productState);
+  const productArr = Object.values(productObj);
 
-    const productObj = useSelector(state => state.productState)
-    const productArr = Object.values(productObj)
-
-
-    const filteredProducts = productArr.filter((product) => {
-
-      if (activeFilter === 'all') {
-        return true;
-      } else if (activeFilter === 'women') {
+  const filteredProducts = productArr.filter((product) => {
+    if (activeFilter === 'all') {
+      return true;
+    } else if (activeFilter === 'women') {
+      if (activeWomenCategory === 'all') {
         return product.category.toLowerCase() === 'women';
-      } else if (activeFilter === 'men') {
-        return product.category.toLowerCase() === 'men';
+      } else {
+        return (
+          product.category.toLowerCase() === 'women' &&
+          product.type.toLowerCase() === activeWomenCategory
+        );
       }
-      return false;
-    });
+    } else if (activeFilter === 'men') {
+      if (activeMenCategory === 'all') {
+        return product.category.toLowerCase() === 'men';
+      } else {
+        return (
+          product.category.toLowerCase() === 'men' &&
+          product.type.toLowerCase() === activeMenCategory
+        );
+      }
+    }
+    return false;
+  });
 
+  const handleFilterChange = (filter) => {
+    setActiveFilter(filter);
+  };
 
+  const handleWomenCategoryChange = (category) => {
+    setActiveWomenCategory(category);
+  };
 
-const handleFilterChange = (filter) => {
-  setActiveFilter(filter);
+  const handleMenCategoryChange = (category) => {
+    setActiveMenCategory(category);
+  };
+
+  useEffect(() => {
+    dispatch(getProductsThunk());
+  }, [dispatch]);
+
+  return (
+    <main className='productListContainer'>
+      <div className='filterButtons'>
+        <button onClick={() => handleFilterChange('all')}>All Products</button>
+        <button onClick={() => handleFilterChange('women')}>Women</button>
+        {activeFilter === 'women' && (
+          <div className='subFilters'>
+            <button onClick={() => handleWomenCategoryChange('jackets')}>Jackets</button>
+            <button onClick={() => handleWomenCategoryChange('pants')}>Pants</button>
+            <button onClick={() => handleWomenCategoryChange('goggles')}>Goggles</button>
+            <button onClick={() => handleWomenCategoryChange('fleece')}>Fleece</button>
+            <button onClick={() => handleWomenCategoryChange('base layers')}>Base Layers</button>
+            <button onClick={() => handleWomenCategoryChange('hoodies')}>Hoodies</button>
+            <button onClick={() => handleWomenCategoryChange('other')}>Other</button>
+          </div>
+        )}
+        <button onClick={() => handleFilterChange('men')}>Men</button>
+        {activeFilter === 'men' && (
+          <div className='subFilters'>
+            <button onClick={() => handleMenCategoryChange('jackets')}>Jackets</button>
+            <button onClick={() => handleMenCategoryChange('pants')}>Pants</button>
+            <button onClick={() => handleMenCategoryChange('goggles')}>Goggles</button>
+            <button onClick={() => handleMenCategoryChange('fleece')}>Fleece</button>
+            <button onClick={() => handleMenCategoryChange('base layers')}>Base Layers</button>
+            <button onClick={() => handleMenCategoryChange('hoodies')}>Hoodies</button>
+            <button onClick={() => handleMenCategoryChange('other')}>Other</button>
+          </div>
+        )}
+      </div>
+
+      <div className='eachProduct'>
+        {filteredProducts.map((product) => (
+          <ProductCard key={product?.id} product={product} />
+        ))}
+      </div>
+    </main>
+  );
 };
-
-    useEffect(() => {
-        dispatch(getProductsThunk())
-      }, [dispatch])
-
-      return (
-        // <main className='productListContainer'>
-
-        //   <div className='eachProduct'>
-        //     {productArr.map((product) => {
-        //     return <ProductCard key={product?.id} product={product}/>
-        //     })}
-        //   </div>
-        // </main>
-
-        <main className='productListContainer'>
-        <div className='filterButtons'>
-          <button onClick={() => handleFilterChange('all')}>All</button>
-          <button onClick={() => handleFilterChange('women')}>Women</button>
-          <button onClick={() => handleFilterChange('men')}>Men</button>
-        </div>
-
-        <div className='eachProduct'>
-          {filteredProducts.map((product) => (
-            <ProductCard key={product?.id} product={product} />
-          ))}
-        </div>
-      </main>
-
-      );
-
-}
 
 export default ProductList;
