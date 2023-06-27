d88fbf4f006e784b2b6a3526af1a873a18a7039e
yining <yiningmaomao@gmail.com>
2023-06-26 21:46:26 -0400

    signup/login css

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
-import './SignupForm.css';
+import "./SignupForm.css";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -22,25 +22,37 @@ function SignupFormPage() {
     e.preventDefault();
     if (password === confirmPassword) {
       setErrors([]);
-      return dispatch(sessionActions.signup({ email, username, password, firstName,lastName }))
+      return dispatch(
+        sessionActions.signup({
+          email,
+          username,
+          password,
+          firstName,
+          lastName,
+        })
+      )
         .catch(async (res) => {
           const data = await res.json();
           if (data && data.errors) setErrors(data.errors);
         });
     }
-    return setErrors(['Confirm Password field must be the same as the Password field']);
+    return setErrors([
+      "Confirm Password field must be the same as the Password field",
+    ]);
   };
 
   return (
-
     <form className="signupform" onSubmit={handleSubmit}>
       <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
       </ul>
 
       <label className="signuplabel">
-        Username
-        <input className="signupinput"
+        <span>Username</span>
+        <input
+          className="signupinput"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
@@ -49,68 +61,75 @@ function SignupFormPage() {
       </label>
 
       <label className="signuplabel">
-        Email
-        <input className="signupinput"
+        <span>Email</span>
+        <input
+          className="signupinput"
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-    </label>
+      </label>
 
-      <label className="signuplabel" >
-        First Name
-        <input className="signupinput"
+      <label className="signuplabel">
+        <span>First Name</span>
+        <input
+          className="signupinput"
           type="text"
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
           required
         />
       </label>
-      <label  className="signuplabel">
-        Last Name
-        <input className="signupinput"
+
+      <label className="signuplabel">
+        <span>Last Name</span>
+        <input
+          className="signupinput"
           type="text"
           value={lastName}
-          onChange={(e) => setAbout(e.target.value)}
-          required
-        />
-      </label>
-
-      <label  className="signuplabel">
-        About yourself
-        <input className="signupinput"
-          type="text"
-          value={about}
           onChange={(e) => setLastName(e.target.value)}
           required
         />
       </label>
 
+      <label className="signuplabel">
+        <span>About yourself</span>
+        <input
+          className="signupinput"
+          type="text"
+          value={about}
+          onChange={(e) => setAbout(e.target.value)}
+          required
+        />
+      </label>
 
       <label className="signuplabel">
-        Password
-        <input className="signupinput"
+        <span>Password</span>
+        <input
+          className="signupinput"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
       </label>
+
       <label className="signuplabel">
-        Confirm Password
-        <input className="signupinput"
+        <span>Confirm Password</span>
+        <input
+          className="signupinput"
           type="password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
           required
         />
       </label>
-      <a   >
-      <button className="signupBtn"  type="submit">Sign Up</button>
-      </a>
-    </form>
 
+      <button className="signupBtn" type="submit">
+        Sign Up
+      </button>
+    </form>
   );
 }
 
